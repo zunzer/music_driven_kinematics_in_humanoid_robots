f8bf8e462a7c9207fcfd4cff8476bfd3cf02dc31
SJasper <64356366+SJasper@users.noreply.github.com>
2021-06-17 21:53:36 +0200

    implemented simple python recording

--- programming-humanoid-robot-in-python/project/sensing.py
+++ programming-humanoid-robot-in-python/project/sensing.py
@@ -1,1 +1,11 @@
 #TODO: Recognize and read the music from computer microphone (played by external device)
+
+import sounddevice as sd
+from scipy.io.wavfile import write
+
+fs = 44100  # Sample rate
+seconds = 10  # Duration of recording
+
+myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+sd.wait()  # Wait until recording is finished
+write('programming-humanoid-robot-in-python/project/recordings/output.wav', fs, myrecording)  # Save as WAV file
