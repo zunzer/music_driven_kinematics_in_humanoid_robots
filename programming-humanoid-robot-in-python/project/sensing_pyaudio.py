b8abc208babc1e10b5bfa4a411c323298d75d514
SJasper <64356366+SJasper@users.noreply.github.com>
2021-06-28 16:04:59 +0200

    Implemented music recording with pyaudio

--- programming-humanoid-robot-in-python/project/sensing_pyaudio.py
+++ programming-humanoid-robot-in-python/project/sensing_pyaudio.py
@@ -1,32 +1,54 @@
-import wave
+'''
+To install pyaudio on windows (https://stackoverflow.com/questions/55936179/trying-to-install-pyaudio-using-pip): 
+- pip install pipwin          #pipwin is the same thing like pip, but with optimized packets for windows 
+- pipwin install pyaudio  
+
+Note: Due to the bad audio quality, you can setup the stereomix device in windows like this:
+https://www.howtogeek.com/howto/39532/how-to-enable-stereo-mix-in-windows-7-to-record-audio/
+
+Based on: https://stackoverflow.com/questions/40704026/voice-recording-using-pyaudio
+'''
+
 import pyaudio
+import wave
 import os
 
-seconds = 10
-pa = pyaudio.PyAudio()
-pa.get_default_host_api_info()
+FORMAT = pyaudio.paInt16
+CHANNELS = 2        #Select number of channels 1/2, depends on microphone 
+RATE = 44100
+CHUNK = 512
+RECORD_SECONDS = 30
+DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-stream_in = pa.open(
-    rate=48000,
-    channels=2,
-    format=pyaudio.paInt16,
-    input=True,                   # input stream flag
-    input_device_index=1,         # input device index
-    frames_per_buffer=1024
-)
+audio = pyaudio.PyAudio()
+print("----------------------record device list---------------------")
+info = audio.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+        if (audio.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device ", i, " - ", audio.get_device_info_by_host_api_device_index(0, i).get('name'))
+print("-------------------------------------------------------------")
 
-# read 5 seconds of the input stream
-input_audio = stream_in.read(seconds * 48000)
+index = int(input("Enter device number and press return: "))  #Note if input function is not working (e.g. in VSCode): Hardcode devicenumber here!
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-output_filename = dir_path+'/recordings/output_pyaudio.wav'
+stream = audio.open(format=FORMAT, channels=CHANNELS,
+                rate=RATE, input=True,input_device_index = index,
+                frames_per_buffer=CHUNK)
+print ("Recording...")
 
-wav_file = wave.open(output_filename, 'wb')
-
-# define audio stream properties
-wav_file.setnchannels(2)        # number of channels
-wav_file.setsampwidth(2)        # sample width in bytes
-wav_file.setframerate(48000)    # sampling rate in Hz
-
-# write samples to the file
-wav_file.writeframes(input_audio)
+Recordframes = []
+for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+    data = stream.read(CHUNK)
+    Recordframes.append(data)
+print ("Recording finished")
+ 
+stream.stop_stream()
+stream.close()
+audio.terminate()
+ 
+waveFile = wave.open(DIR_PATH + '/recordings//pyaudio_recording.wav', 'wb')
+waveFile.setnchannels(CHANNELS)
+waveFile.setsampwidth(audio.get_sample_size(FORMAT))
+waveFile.setframerate(RATE)
+waveFile.writeframes(b''.join(Recordframes))
+waveFile.close()
