70659ce8556501fd5a4c05b2f766d357424d5780
SJasper <64356366+SJasper@users.noreply.github.com>
2021-06-28 19:21:50 +0200

    added RoboDance test keyframes

--- programming-humanoid-robot-in-python/joint_control/keyframes/__init__.py
+++ programming-humanoid-robot-in-python/joint_control/keyframes/__init__.py
@@ -5,6 +5,7 @@ from .leftBackToStand import leftBackToStand
 from .leftBellyToStand import leftBellyToStand
 from .rightBackToStand import rightBackToStand
 from .rightBellyToStand import rightBellyToStand
+from .RoboDance import RoboDance
 
 from .hello import hello
 from .wipe_forehead import wipe_forehead
