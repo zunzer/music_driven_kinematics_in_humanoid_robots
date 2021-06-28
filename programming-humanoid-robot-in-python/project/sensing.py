68cd09360175d88d50751db0a0de8567367caa7d
Cornelius <cornelius.braun@posteo.de>
2021-06-28 11:14:48 +0200

    convolutional network architecture for music recognition

--- programming-humanoid-robot-in-python/project/sensing.py
+++ programming-humanoid-robot-in-python/project/sensing.py
@@ -5,7 +5,7 @@ from scipy.io.wavfile import write
 import os
 
 fs = 44100  # Sample rate
-seconds = 10  # Duration of recording
+seconds = 30  # Duration of recording
 
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
 sd.wait()  # Wait until recording is finished
