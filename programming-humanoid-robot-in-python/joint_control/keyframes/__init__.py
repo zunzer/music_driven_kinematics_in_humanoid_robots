4af0329056f8f399bd35edff61af295ed7555387
SJasper <64356366+SJasper@users.noreply.github.com>
2021-06-29 23:57:59 +0200

    Add disco import in init.py

--- programming-humanoid-robot-in-python/joint_control/keyframes/__init__.py
+++ programming-humanoid-robot-in-python/joint_control/keyframes/__init__.py
@@ -6,6 +6,7 @@ from .leftBellyToStand import leftBellyToStand
 from .rightBackToStand import rightBackToStand
 from .rightBellyToStand import rightBellyToStand
 from .RoboDance import RoboDance
+from .disco import disco
 
 from .hello import hello
 from .wipe_forehead import wipe_forehead
