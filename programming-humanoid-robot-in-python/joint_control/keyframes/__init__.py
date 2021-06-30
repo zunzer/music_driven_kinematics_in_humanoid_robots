20aeea6d5f24e8033ebe8fbe3e6d28b7ff219b9e
eschkola <e.schulenburg@campus.tu-berlin.de>
2021-06-30 11:56:44 +0200

    added new python keyframes and added to __init__

--- programming-humanoid-robot-in-python/joint_control/keyframes/__init__.py
+++ programming-humanoid-robot-in-python/joint_control/keyframes/__init__.py
@@ -5,7 +5,9 @@ from .leftBackToStand import leftBackToStand
 from .leftBellyToStand import leftBellyToStand
 from .rightBackToStand import rightBackToStand
 from .rightBellyToStand import rightBellyToStand
-from .RoboDance import RoboDance
+from .disco import disco
+from .roboDance import robotDance
+from .classic import classic
 
 from .hello import hello
 from .wipe_forehead import wipe_forehead
