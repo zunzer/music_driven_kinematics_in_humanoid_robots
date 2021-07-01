457771318a7c63af601638b902562689fa56a11d
Cornelius <cornelius.braun@posteo.de>
2021-07-01 19:27:22 +0200

    wrote sense-think-act structure; now only integration with audio sensing is missing

--- programming-humanoid-robot-in-python/project/dancing.py
+++ programming-humanoid-robot-in-python/project/dancing.py
@@ -38,25 +38,44 @@ class DancingAgent(PostureRecognitionAgent):
         # for now use a random file from the genres train data -- TODO: read the music from sensing in the future
         self.recognized_flag = False
         #self.music_data = extract_song_features("../project/recordings/output_normalized.wav") # TODO: we need 30s long recordings
-        self.music_data = extract_song_features("../project/music_recognition/NN_classification/genres/pop/pop.00000.wav") # TODO: we need 30s long recordings
+        #self.music_data = extract_song_features("../project/music_recognition/NN_classification/genres/pop/pop.00000.wav") # TODO: we need 30s long recordings
 
         print("done setting up everything")
+
+    def listen(self):
+        """
+        This records 30s of music and returns it as music_ data
+        TODO: @Severin, hier sollte dann ein Funktionsaufruf rein, der dein sensing_normalized aufruft oder so
+        """
+        print("sensing")
+        music_data = extract_song_features("../project/music_recognition/NN_classification/genres/pop/pop.00000.wav")
+        return music_data
 
     def think(self, perception):
         """
         get the file from sensing, predict genre from it and set keyframes
         """
         if not self.recognized_flag:
+            self.music_data = self.listen()
             music_input = self.music_data[np.newaxis, :]
             prediction = self.music_classifier.predict(music_input)
             music_genre = self.genres[prediction[0]]
-            #music_genre = "pop"
             print(f"recognized the following genre: {music_genre}!")
 
             # TODO: put this into separate act function
-            self.keyframes = self.keyframes_dictionary[music_genre]
+            keyframes = self.keyframes_dictionary[music_genre]
+            self.dance(keyframes)
             self.recognized_flag = True
+
         return super(DancingAgent, self).think(perception)
+
+    def dance(self, keyframes):
+        """
+        This is our acting function. It sets new keyframes to animate if not done yet
+        """
+        if self.recognized_flag:
+            return
+        self.keyframes = keyframes
 
 
 if __name__ == '__main__':
